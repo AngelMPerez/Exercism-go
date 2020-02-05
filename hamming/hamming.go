f9e765ba417017734b04d92f6a5ee759493e1455
Angel Perez <angelmperezcarranza@gmail.com>
2020-02-05 08:44:04 -0700

    new

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,18 @@
 package hamming
+import "errors"
 
 func Distance(a, b string) (int, error) {
+	var index int
+	var x=0 
+	var y="false"
+	if(len(a)!=len(b)){
+		y="true"
+	}else{
+		for index=0;index<len(a);index++{
+			if(a[index]!=b[index]){
+				x=x+1
+			}
+		}
+	}
+	return x,errors.New(y)
 }
