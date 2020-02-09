c05cb6a38ab812d2b9146445410ae55b628fa223
Angel Perez <angelmperezcarranza@gmail.com>
2020-02-09 10:52:57 -0700

    finish

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -7,10 +7,7 @@ package twofer
 
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+	
 	if(len(name)==0){
 		return "One for you, one for me."
 	}else{
