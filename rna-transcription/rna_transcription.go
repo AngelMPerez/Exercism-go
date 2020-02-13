076815289445fad2a91f0a8faebf2295a5d0964e
Angel Perez <angelmperezcarranza@gmail.com>
2020-02-12 17:34:29 -0700

    in progress

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,16 @@
 package strand
 
 func ToRNA(dna string) string {
+
+	var x="" 
+	for i:=0;i<len(dna);i++{
+		s := string([]byte{dna[i]})
+			switch{
+			case s == "A": x= "U"
+			case s == "C": x= "G"
+			case s == "T": x= "A"
+			case s == "G": x= "C"
+		}
+	}	
+	return x
 }
