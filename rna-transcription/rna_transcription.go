0264717f49b7173207e831c7aaf0f510ee0249a9
Angel Perez <angelmperezcarranza@gmail.com>
2020-02-12 17:40:22 -0700

    finish

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,16 +1,17 @@
 package strand
+import "bytes"
 
 func ToRNA(dna string) string {
 
-	var x="" 
+	var x bytes.Buffer
 	for i:=0;i<len(dna);i++{
 		s := string([]byte{dna[i]})
 			switch{
-			case s == "A": x= "U"
-			case s == "C": x= "G"
-			case s == "T": x= "A"
-			case s == "G": x= "C"
+			case s == "A": x.WriteString("U")
+			case s == "C": x.WriteString("G")
+			case s == "T": x.WriteString("A")
+			case s == "G": x.WriteString("C")
 		}
 	}	
-	return x
+	return x.String()
 }
